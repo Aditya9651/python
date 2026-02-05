fa7f5cc7746214b05d53797772d7454070371498
Aditya9651 <kumarianeeta29@gmail.com>
2026-02-05 23:43:49 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,6 @@
+employee={"name":"John","age":30,"city":"New York"}
+print(type(employee))
+print("printing employee dictionary")
+print("name:%s"%employee["name"])
+print("age:%d"%employee["age"])
+print("city:%s"%employee["city"])
